9f322724881a9bb5c26bd5fa7854d88953788973
GustavoMCF <gustavomcferreira1@hotmail.com>
2020-07-28 13:16:38 -0300

    Commit aula2

--- src/index.js
+++ src/index.js
@@ -1,14 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
+
+// Colocar um jogo nessa pagina
+const Pagina404 = () => (<div>Página 404</div>)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/cadastro/video" component={CadastroVideo}/>
+        <Route path="/cadastro/categoria" component={CadastroCategoria}/>
+        <Route component={Pagina404} />
+      </Switch>
+    </BrowserRouter>,
   document.getElementById('root')
 );
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+<React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  */
+
 /*
 Update automágico (Hot reload não é hot roll)
 JSX É IGUAL [J]AVA[S]CRIPT [X]ML
