ebaa72beef4f479f156b558ace6da061e39f77a2
GustavoMCF <gustavomcferreira1@hotmail.com>
2020-07-28 09:12:10 -0300

    Setup inicial do projeto

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+/*
+Update automágico (Hot reload não é hot roll)
+JSX É IGUAL [J]AVA[S]CRIPT [X]ML
+*/
